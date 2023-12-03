e54f9ed43f2ee113b54223c016fd46fa96a00c83
katheryn11 <katherynbr97@gmail.com>
2023-12-02 21:02:08 -0600

    conectando la base de datos para que funciones los reportes de empleados

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 /**conexion a BD */
-$usuario  = "root";
-$password = "";
-$servidor = "localhost";
-$basededatos = "supermercado";
+$usuario  = "admin";
+$password = "Temporal1";
+$servidor = "34.125.240.38";
+$basededatos = "viajerodb";
 $con = mysqli_connect($servidor, $usuario, $password) or die("No se ha podido conectar al Servidor");
 mysqli_query($con,"SET SESSION collation_connection ='utf8_unicode_ci'");
 $db = mysqli_select_db($con, $basededatos) or die("Upps! Error en conectar a la Base de Datos");
